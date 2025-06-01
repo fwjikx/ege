4963d1cc6a124ba774a9052691f0dbcaeac9d16b
fwjikx <123657572+fwjikx@users.noreply.github.com>
2025-06-01 09:27:19 +0300

    Update №16.py

--- №16.py
+++ №16.py
@@ -1,14 +1,35 @@
-from functools import lru_cache
+#cпособ 1 - на слабых компах не потянет
+from sys import *
+setrecursionlimit(1000000)
 
-
-@lru_cache(None)
 def f(n):
-    if n == 1: return 1
-    if n > 1:
-        return n * f(n - 1)
+    if n < 5:
+        return n
+    if n >= 5:
+        return 2 * n * f(n - 4)
 
+print((f(13766) - 9 * f(13762)) // f(13758))
 
-for n in range(2023):
-    f(n)
+#2 способ - кэшируем результаты
+from functools import *
+@lru_cache(10000) - "None лучше не ставить"
 
-print(f(2024) // (f(2023) - f(2022)))
+def f(n):
+    if n < 5:
+        return n
+    if n >= 5:
+        return 2 * n * f(n - 4)
+
+for i in range(14000): #заполняем кэш
+    f(i)
+
+print((f(13766) - 9 * f(13762)) // f(13758))
+
+#3 способ - мемоизация через список
+f = [0] * 14000
+for n in range(len(f)):
+    if n < 5:
+        f[n] = n
+    if n >= 5:
+        f[n] = 2 * n * f[n - 4]
+print((f[13766]- 9 * f[13762]) // f[13758])
