0f637add3cd64e7b9c4c0753f103cca20084de98
fwjikx <123657572+fwjikx@users.noreply.github.com>
2025-06-01 09:37:46 +0300

    Update № 19, 20, 21.py

--- № 19, 20, 21.py
+++ № 19, 20, 21.py
@@ -1,32 +1,36 @@
 # нечет - ход пети, чет - ход вани
 
-# type1(№19-21) ОДНА КУЧА
-
-def f(s, m):
-    if s >= 30: return m % 2 == 0
+#1 куча
+def f(s,m): #s - кол-во камней в куче, m - кол-во ходов
+    if s >= 132: return m % 2 == 0
     if m == 0: return 0
-    h = [f(s + 1, m - 1), f(s * 2, m - 1)]
-    return any(h) if m % 2 != 0 else all(h) #  ДЛЯ 19) any, а не all, когда в условии сказано, что после неудачного 1-ого хода Пети
+    h = [f(s + 3,m - 1), f(s + 6, m - 1), f(s * 3, m - 1)]
+    return any(h) if m % 2 != 0 else all(h) #для 19 номера all может поменяться на any если в вопросе звучит "НЕУДАЧНЫЙ ХОД"
 
+print('#19', min([s for s in range(1,132) if f(s,2)])) #игра заканчивается за 2 хода (победа вани)
+print('#20', [s for s in range(1,132) if not(f(s,1)) and f(s,3)]) #игра не заканчивается за 1 ход, но заканчивается на 3 ходу (победа пети)
+print('#21', min([s for s in range(1,132) if not(f(s,2)) and f(s,4)])) #игра не заканчивается за 2 хода, но заканчивается на 4 ходу (победа вани)
 
-print('19)', [s for s in range(1, 30) if f(s, 2)])
-print('20)', [s for s in range(1, 30) if not f(s, 1) and f(s, 3)])
-print('21)', [s for s in range(1, 30) if not f(s, 2) and f(s, 4)])
-'''
-
-
-# type2(№19-21) ДВЕ КУЧИ
-def f(a, b, m):
-    if a + b <= 20: return m % 2 == 0
+#2 кучи
+def f(x,y,m): #x = 1 куча , y = 2 куча , m - кол-во ходов
+    if (x + y) >= 77: return m % 2 == 0
     if m == 0: return 0
-    h = [f(a - 1, b, m - 1), f(a, b - 1, m - 1), f(a // 2, b, m - 1), f(a, b // 2, m - 1)]
-    return any(h) if m % 2 != 0 else all(h)  # 19) any, а не all, т.к. в условии сказано, что после неудачного 1-ого хода Пети
+    h = [f(x + 3, y, m - 1), f(x * 3, y , m - 1), f(x , y + 3, m - 1), f(x , y * 3, m - 1)]
+    return any(h) if m % 2 != 0 else all(h) #вот тут второе all меняем на any только в 19 номере если вопрос при "неудачном ходе"
+
+print('#19', min([s for s in range(1,65) if f(12,s,2)]))
+print('#20', [s for s in range(1,65) if not(f(12,s,1)) and f(12,s,3)])
+print('#21', [s for s in range(1,65) if not(f(12,s,2)) and f(12,s,4)])
+
+#отрезки в играх
+def f(x, m):
+    if 105 >= x >= 97: return m % 2 == 0
+    if x > 105: return m % 2 != 0
+    if m == 0: return 0
+    h = [f(x + 3, m - 1), f(x + 5, m - 1), f(x * 3, m - 1)]
+    return any(h) if m % 2 != 0 else all(h)
 
 
-
-print('19)', [s for s in range(10, 100) if f(10, s, 2)])
-print('20)', [s for s in range(10, 100) if not f(10, s, 1) and f(10, s, 3)])
-print('21)', [s for s in range(10, 100) if not f(10, s, 2) and f(10, s, 4)])
-
-'''
-
+print('#19', [s for s in range(1, 97) if f(s, 2)])
+print('#20', [s for s in range(1, 97) if not (f(s, 1)) and f(s, 3)])
+print('#21', [s for s in range(1, 97) if not (f(s, 2)) and f(s, 4)])
